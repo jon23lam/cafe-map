8694afcf38346f3bb2c74dbbebdb7ac783c53587
Jon Lam <jonl@wingmateapp.com>
2023-07-27 21:56:24 -0400

    Map functionality outline

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,30 +1,45 @@
 import React, { useState, useEffect } from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import cafeJson from "../database/CafeLocations.json"
+import { Cafe } from '../types/Cafe';
+import { 
+  GoogleMap, 
+  Marker,
+  MarkerClusterer,
+  InfoWindowF,
+  useJsApiLoader 
+} from '@react-google-maps/api';
 
 const containerStyle = {
-  width: '400px',
-  height: '400px'
+  width: '100vw',
+  height: '100vh'
 };
 
 const center = {
-  lat: -3.745,
-  lng: -38.523
+  lat: 43.6405,
+  lng: -79.3750
 };
 
+const INITIAL_ZOOM = 13
+const cafeLocations: Array<Cafe> = cafeJson.data.cafes
+
+
 export function MapPage() {
-  const configval = process.env.REACT_APP_CAFE_MAP_KEY as string
-  console.log(configval)
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: configval
-  })
+  const mapsApiKey = process.env.REACT_APP_CAFE_MAP_KEY as string
 
   const [map, setMap] = useState<google.maps.Map | null>(null);
 
+  const [activeMarker, setActiveMarker] = useState<number| null>(null);
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: mapsApiKey
+  })
+
+
   const onLoad = React.useCallback(function callback(map: google.maps.Map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
+    map.setZoom(INITIAL_ZOOM)
+
 
     setMap(map)
   }, [])
@@ -33,16 +48,38 @@ export function MapPage() {
     setMap(null)
   }, [])
 
+  const handleActiveMarker = (marker: number) => {
+    if (marker === activeMarker) {
+      return;
+    }
+    setActiveMarker(marker);
+  };
+
   return isLoaded ? (
       <GoogleMap
         mapContainerStyle={containerStyle}
+        onClick={() => setActiveMarker(null)}
         center={center}
-        zoom={10}
+        zoom={INITIAL_ZOOM}
         onLoad={onLoad}
         onUnmount={onUnmount}
       >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
+          {cafeLocations.map(({ id, name, address, type, lat, lng }) => (
+            <Marker
+              key={id}
+              position={{lat: lat, lng: lng}}
+              onClick={() => handleActiveMarker(id)}
+            >
+              {activeMarker === id ? (
+                <InfoWindowF 
+                  position={{lat: lat, lng: lng}}
+                  onCloseClick={() => setActiveMarker(null)}
+                >
+                  <div>{`${name} (${type})`}</div>
+                </InfoWindowF>
+              ) : null}
+            </Marker>
+          ))}
       </GoogleMap>
   ) : <></>
 
---
+++ src/types/Cafe.ts
@@ -0,0 +1,8 @@
+export interface Cafe {
+  id: number,
+  name: string,
+  address: string,
+  type: string,
+  lat: number,
+  lng: number
+}
