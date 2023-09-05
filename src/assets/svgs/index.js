a52c40f7d32b748494ac9028b4fdf5a319d59403
Jon Lam <jonlam@Jons-MacBook-Pro.local>
2023-09-04 21:14:25 -0400

    adding back env variable

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -1,15 +1,10 @@
+import React from "react";
+import { ReactComponent as IceCreamSvg } from "./ice-cream-17.svg";
 
-import React from 'react'
-// export { default as CoffeeCupSvg } from "./Coffee_cup_flat.svg";
 export { default as IceCreamPin } from "./ice-cream-17.svg";
 
-
-
-
-export { ReactComponent as IceCreamSvg } from "./ice-cream-17.svg"
-import { ReactComponent as IceCreamSvg } from "./ice-cream-17.svg"
-
+export { ReactComponent as IceCreamSvg } from "./ice-cream-17.svg";
 
 export const SVG_MAP = {
-  "Sweet Treats": <IceCreamSvg />
+  "Sweet Treats": <IceCreamSvg />,
 };
